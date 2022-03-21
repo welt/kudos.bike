4a20caead9fe1713bbb5f321e15a08cdee95bdda
welt <welt@mac.com>
2022-03-21 22:59:02 +0000

    adds race 1 2022 leaderbaords

--- src/leaderboards/2021/club/index.11ty.js
+++ src/leaderboards/2021/club/index.11ty.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable class-methods-use-this */
-const formatPointsColumns = require('../../_lib/formatPointsColumns');
-const navSecondary = require('../../_includes/navSecondary');
-const ordinalSuffixOf = require('../../_lib/ordinalSuffixOf');
+const formatPointsColumns = require('../../../_lib/formatPointsColumns');
+const navSecondary = require('../../../_includes/navSecondary');
+const ordinalSuffixOf = require('../../../_lib/ordinalSuffixOf');
 
 class Results {
   constructor() {
@@ -11,12 +11,19 @@ class Results {
 
   get data() {
     return {
-      title: 'Kudos TT Series, 2021 men\'s leaderboard',
-      metaDescription: 'Kudos Time Trial Series, 2021 men\'s leaderboard',
+      title: 'Kudos TT Series, 2021 club leaderboard',
+      metaDescription: 'Kudos Time Trial Series, 2021 club leaderboard',
       styles: 'hide-bg',
     };
   }
 
+  /**
+   * Gets the first event object and sends it to the formatPointsColumns()
+   * to use its object keys as table column headers for all the others.
+   *
+   * @param {object} obj - results object (table row)
+   * @returns {string}
+   */
   formatPointsColumnHeaders(obj) {
     const { results } = obj;
     const [firstResult] = results;
@@ -27,8 +34,7 @@ class Results {
     const { results } = obj;
     return results.map((result, index) => `
       <tr>
-        <td data-label="Name" class="name" data-ordinal="${ordinalSuffixOf(index + 1)}">${result.Firstname} ${result.Lastname}</td>
-        <td data-label="Club" class="club">${result.Club}</td>
+        <td data-label="Club" class="club" data-ordinal="${ordinalSuffixOf(index + 1)}">${result.Club}</td>
         ${this.formatPointsColumns(result)}
         <td data-label="Total Points" class="total-points">${result.TotalPoints}</td>
       </tr>
@@ -43,7 +49,6 @@ class Results {
       <table aria-live="polite" id="results-table" class="table-responsive hide-columns">
         <caption>Results</caption>
         <thead>
-          <th class="name">Name</th>
           <th class="club">Club</th>
           ${this.formatPointsColumnHeaders(data)}
           <th class="points">Total Points</th>
--- src/leaderboards/2022/men/index.11ty.js
+++ src/leaderboards/2022/men/index.11ty.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable class-methods-use-this */
-const formatPointsColumns = require('../../_lib/formatPointsColumns');
-const navSecondary = require('../../_includes/navSecondary');
-const ordinalSuffixOf = require('../../_lib/ordinalSuffixOf');
+const formatPointsColumns = require('../../../_lib/formatPointsColumns');
+const navSecondary = require('../../../_includes/navSecondary');
+const ordinalSuffixOf = require('../../../_lib/ordinalSuffixOf');
 
 class Results {
   constructor() {
@@ -11,19 +11,12 @@ class Results {
 
   get data() {
     return {
-      title: 'Kudos TT Series, 2021 club leaderboard',
-      metaDescription: 'Kudos Time Trial Series, 2021 club leaderboard',
+      title: 'Kudos TT Series, 2021 men\'s leaderboard',
+      metaDescription: 'Kudos Time Trial Series, 2021 men\'s leaderboard',
       styles: 'hide-bg',
     };
   }
 
-  /**
-   * Gets the first event object and sends it to the formatPointsColumns()
-   * to use its object keys as table column headers for all the others.
-   *
-   * @param {object} obj - results object (table row)
-   * @returns {string}
-   */
   formatPointsColumnHeaders(obj) {
     const { results } = obj;
     const [firstResult] = results;
@@ -34,7 +27,8 @@ class Results {
     const { results } = obj;
     return results.map((result, index) => `
       <tr>
-        <td data-label="Club" class="club" data-ordinal="${ordinalSuffixOf(index + 1)}">${result.Club}</td>
+        <td data-label="Name" class="name" data-ordinal="${ordinalSuffixOf(index + 1)}">${result.Firstname} ${result.Lastname}</td>
+        <td data-label="Club" class="club">${result.Club}</td>
         ${this.formatPointsColumns(result)}
         <td data-label="Total Points" class="total-points">${result.TotalPoints}</td>
       </tr>
@@ -49,6 +43,7 @@ class Results {
       <table aria-live="polite" id="results-table" class="table-responsive hide-columns">
         <caption>Results</caption>
         <thead>
+          <th class="name">Name</th>
           <th class="club">Club</th>
           ${this.formatPointsColumnHeaders(data)}
           <th class="points">Total Points</th>
