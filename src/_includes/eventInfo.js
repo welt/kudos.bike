700345ed2cb554b67b686b50f6a34d1d72c5a227
welt <welt@mac.com>
2022-03-21 22:55:22 +0000

    hides old races from home page

--- src/_includes/eventInfo.js
+++ src/_includes/eventInfo.js
@@ -4,21 +4,27 @@
 const eventData = require('../_lib/eventData');
 const eventJsonLd = require('../_lib/eventJsonLd');
 
+const isOld = (raceDateObj) => new Date() > raceDateObj;
+
 module.exports = function eventInfo(data) {
   const event = ('event' in data) ? data.event : data;
-  return `
-    ${eventJsonLd(event)}
-    <div id="event-${event.id}" class="event" data-calendar-item='${eventData(event)}'>
-      <p>${event.Event}</p>
-      <h2>${event.Name}</h2>
-      <div class="event-details">
-        <p><time datetime="${event.Date}">${new Date(event.Date).toDateString()}, ${new Date(event.Date).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })}</time></p>
-        <p>Course: ${event.Course} ${event.CourseInfo}</p>
-        <p>Distance: ${event.Distance}</p>
-        <p>Price: £${event.Price}</p>
-        <p>Closing date for entries: ${new Date(event.ClosingDate).toDateString()}</p>
-        <p>Details: ${(event.EventDetails) ? `<a class="event-link" href="${event.EventDetails}">${event.EventDetails}</a>` : ''}</p>
-      </div>
-    </div>
-    `;
+  const raceHappened = isOld(new Date(event.Date));
+  if (!raceHappened) {
+    return `
+     ${eventJsonLd(event)}
+     <div id="event-${event.id}" class="event" data-calendar-item='${eventData(event)}'>
+       <p>${event.Event}</p>
+       <h2>${event.Name}</h2>
+       <div class="event-details">
+         <p><time datetime="${event.Date}">${new Date(event.Date).toDateString()}, ${new Date(event.Date).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })}</time></p>
+         <p>Course: ${event.Course} ${event.CourseInfo}</p>
+         <p>Distance: ${event.Distance}</p>
+         <p>Price: £${event.Price}</p>
+         <p>Closing date for entries: ${new Date(event.ClosingDate).toDateString()}</p>
+         <p>Details: ${(event.EventDetails) ? `<a class="event-link" href="${event.EventDetails}">${event.EventDetails}</a>` : ''}</p>
+       </div>
+     </div>
+     `;
+  }
+  return '';
 };
